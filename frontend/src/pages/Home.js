4300939be9da9730e77555565a401e0db016f88a
Davi Giroux Alvarenga <dalvarenga@ni-35682-5p.uolcorp.intranet>
2021-01-29 15:16:40 -0400

    adicionando texto na home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,6 +10,7 @@ function Home() {
     return (
         <div>
             <h1>Bem vindo!</h1>
+            <p>Esse é um app simples com a finalidade de ajudar você a ter o controle dos seus gastos mensais :D</p>
             <TextoDeBoasVindas>
                 Você pode <Link to="/login">logar na sua conta</Link> ou <Link to="/registrar">criar uma conta do zero!</Link>
             </TextoDeBoasVindas>
