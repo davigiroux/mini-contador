9666d9327aa0622c8b1993d40537fa3b6b4746da
Davi Giroux Alvarenga <dalvarenga@ni-35682-5p.uolcorp.intranet>
2021-01-29 13:59:40 -0400

    Adicionando porta de servidor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
 import App from './app';
 import AutenticacaoController from './controllers/AutenticacaoController';
 import ContaController from './controllers/ContaController';
- 
+
+const porta = Number(process.env.PORT) || 3000;
+
 const app = new App(
   [
       new AutenticacaoController(),
       new ContaController()
   ],
-  5000,
+  porta,
 );
  
 app.listen();
